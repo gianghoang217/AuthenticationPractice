7280349b5d729e667b7ebb724be2d3263d55d203
GiangHoang <giang@tenpoint7.com>
2021-03-29 17:01:46 +0700

    add server api url

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ passport.use(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/secrets",
+      callbackURL: `${process.env.SERVER_API_URL}/auth/google/secrets`,
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
       passReqToCallback: true
     },
